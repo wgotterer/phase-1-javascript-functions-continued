fd8e95002ae1cf4b2b0f6be3eaa4befa70bc953e
William Gotterer <william.gotterer@gmail.com>
2021-09-21 09:33:51 -0400

    commit tree ad354d40ab128c3bd95b363b9df4ad26c6eed017

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+saturdayFun();
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*"){
+    return function(type="special"){
+        return `You are ${flair}${type}${flair}!`
+    };
+}
+
+wrapAdjective("*")("a dedicated programmer"); 
+
+
+
+// function outer(greeting, msg = "It's a fine day to learn") {
+//     return function (name, lang = "Python") {
+//       return `${greeting}, ${name}! ${msg} ${lang}`;
+//     };
+//   }
+  
+//   outer("Hello")("student", "JavaScript");
+
+
+//   //=> "Hello, student! It's a fine day to learn JavaScript"
+  
